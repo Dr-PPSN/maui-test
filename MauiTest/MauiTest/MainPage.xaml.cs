97b48892e07095974498baf2e4fe26ab27023054
Dr-PPSN <kairohrpasser@gmail.com>
2022-09-29 19:17:06 +0200

    init
    first upload

---
+++ MauiTest/MauiTest/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiTest;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+        //get screen size
+        var width = DeviceDisplay.MainDisplayInfo.Width;
+        var height = DeviceDisplay.MainDisplayInfo.Height;
+
+        WebView1.WidthRequest = width-10;
+        WebView1.HeightRequest = height-10;
+    }
+}
